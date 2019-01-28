6bf1b90b8425e6feac072119fce6ad081fe7f5f2
Bizoi Cristina <bizoi.cristina95@gmail.com>
2019-01-28 21:16:34 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<head>
+<meta charset = "UTF-8">
+    <meta name = "viewpoint" content = "width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tutorial HTML CSS</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,800" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link rel="stylesheet" href="./public/css/style.css">
+</head>
+
+<body>
+    <div class="section-1">
+       <div class="container">
+            <header>
+                    <div class="logo"> Tanatos<span id="studio">Studio<span></div>
+                    <div class="meniu"> Meniu <i class="fas fa-bars"></i> </div>
+            </header>
+            <section>
+                    <h1>WE DESIGN <span class="things">THINGS</span></h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                    </p>
+                    <a href="#" class="butonstanga roundcorner">Hire us </a>
+                    <a href="#" class="butondreapta roundcorner">Our work</a>
+            </section>
+            
+        </div>
+    </div>
+    <div class="section-2">
+        <div class=" servicii">
+            <div class="container">
+                <div class="col-20 margine clearfix">
+                    <div class="imagine">
+                        <img src="./public/imgs/imprimanta.gif" alt="printdesign">
+                    </div>
+                    <div class="titluservicii">
+                        <h4>PRINT DESIGN</h4>
+                    </div>
+                    <div class="descriereservicii">
+                        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qul officia</p>
+                    </div>
+                </div>
+                <div class="col-20 margine clearfix">
+                    <div class="imagine">
+                        <img src="./public/imgs/monitor.gif" alt="webdesign">
+                    </div>
+                    <div class="titluservicii">
+                        <h4>WEB DESIGN</h4>
+                    </div>
+                    <div class="descriereservicii">
+                        <p>Incididunt ut labore et dolore magna aliqua. Ut enim ad minim venseim</p>
+                    </div>
+                </div>
+                <div class="col-20 margine clearfix">
+                    <div class="imagine">
+                        <img src="./public/imgs/phone.gif" alt="appdesign">
+                    </div>
+                    <div class="titluservicii">
+                        <h4>APP DESIGN</h4>
+                    </div>
+                    <div class="descriereservicii">
+                        <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex eacomos</p>
+                    </div>
+                </div>
+                <div class="col-20 clearfix ">
+                    <div class="imagine">
+                        <img src="./public/imgs/instrumentedescris.gif" alt="logodesign">
+                    </div>
+                    <div class="titluservicii">
+                        <h4>LOGO DESIGN</h4>
+                    </div>
+                    <div class="descriereservicii">
+                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="newsletter">
+            <div class="container">
+                <form action="index.php" method="post" class="formular1">
+                    <input type="email" class="email campformular roundcorner" name="email" placeholder="Enter your email adress for newsletter">
+                    <input type="button" class="buton subscribe roundcorner" value="Subscribe">  
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="section-3">
+        <div class="container">
+            <div class="works textgray">
+                <div class="titlu">OUR <span class="orangeword">WORKS</span></div>
+                <p class="descriere">Ut enim ad minim veniam, quis nostrud exercitation ullamco labo</p>
+            </div>
+                <div class="col-30">
+                    <div class="work">
+                        <img src="./public/imgs/workstableta.gif">
+                    </div>
+                </div>
+                <div class="col-30">
+                    <div class="work">
+                        <img src="./public/imgs/worksecran.gif">
+                    </div>
+                </div>
+                <div class="col-30">
+                    <div class="work">
+                        <img src="./public/imgs/workslaptop.gif">
+                    </div>
+                </div>
+                <div class="col-30">
+                    <div class="work">
+                        <img src="./public/imgs/workstableta.gif">
+                    </div>
+                </div>
+                <div class="col-30">
+                    <div class="work">
+                        <img src="./public/imgs/worksipad.gif" class=>
+                    </div>
+                </div>
+                <div class="col-30">
+                    <div class="work abazoo">
+                        <img src="./public/imgs/worksbuton.gif" class="imaginefundal">
+                            <div class="overlay">
+                                    <span class="abazoowebsite">ABAZOO WEBSITE</span>
+                                    <p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+                                    <form>
+                                        <input type="button" class="butonalb roundcorner" id="project" value="SHOW PROJECT">
+                                    </form>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <a href="#" class="show roundcorner" >SHOW MORE</a>
+        </div>
+    </div>
+    <div class="section-4">
+        <div class="container">
+            <div class="section-4-items">
+                <div class="slideshow-container">
+                    <div class="imagineangajat">
+                        <img src="./public/imgs/Avatar.gif">
+                    </div>
+                    <p class="italic">These guys are incredible! I get my project in 10 days and it w</p>
+                    <p>John Smith</p>
+            <!-- Full-width images with number and caption text -->
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="section-5">
+        <div class="container">
+            <div class="section-5-items">
+                <div class="team textgray">
+                    <div class="titlu"> OUR <span class="orangeword">TEAM</span></div>
+                    <p class="descriere">Ut enim ad minim veniam, quis nostrud exercitation ullamco labo</p>
+                </div>
+                <div class="row">
+                    <div class="col-25 angajat">
+                        <div  class="angajatimagine">
+                            <img src="./public/imgs/angajat1.gif">
+                        </div>
+                            <h4>Catherina Gail</h4> 
+                            <div class="retele"><i class="fab fa-facebook-f"></i> <i class="fab fa-twitter"></i> <i class="fab fa-google-plus"></i></div>
+                            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odi</p>
+                    </div>
+                    <div class="col-25 angajat">
+                        <div  class="angajatimagine">
+                            
+                            <img src="./public/imgs/angajat1.gif">
+                        </div>
+                            <h5>Harvey Rube</h5> 
+                            <div class="retele"><i class="fab fa-facebook-f"></i> <i class="fab fa-twitter"></i> <i class="fab fa-google-plus"></i></div>
+                            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odi</p>
+                        
+                    </div>
+                    <div class="col-25 angajat">
+                        <div  class="angajatimagine">
+                            <img src="./public/imgs/angajat1.gif" class="zoom">
+                        </div>
+                            <h5>Harvey Rube</h5> 
+                            <div class="retele"><i class="fab fa-facebook-f"></i> <i class="fab fa-twitter"></i> <i class="fab fa-google-plus"></i></div>
+                            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odi</p>
+                    </div>
+                    <div class="col-25 angajat">
+                        <div  class="angajatimagine">
+                            <img src="./public/imgs/angajat1.gif">
+                        </div>
+                            <h5>Harvey Rube</h5> 
+                            <div class="retele"><i class="fab fa-facebook-f"></i> <i class="fab fa-twitter"></i> <i class="fab fa-google-plus"></i></div>
+                            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odi</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="section-6">
+        <div class="container">
+            <div class="section-6-items">
+                    <span class="titlu">SAY <span class="orangeword">HELLO</span></span>
+                    <p class="descriere">Excepteur sint occaecat cupidatat non proident, sunt in culpa q cupidatat non proident, sunt in culpa q</p>
+                <form class="elementeformular">
+                    <div class="row">
+                        <input type="email" class="col-50 campformular spatiere roundcorner" placeholder="Your Email">
+                        <input type="text" class="col-50 campformular roundcorner" placeholder="Subject">
+                    </div>
+                    <textarea id="textarea" class="roundcorner" placeholder="Your message"></textarea>
+                    <div class="row">
+                        <input type="button" value="HIRE US" class="buton roundcorner col-25 hireus spatiere">
+                        <div class="seealso col-50"></div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="container">
+            <p class="copyright">© 2015 COPYRIGHT DESIGN STUDIO</p>
+            <p class="designby">DESIGNED BY SYMU.COM</p>
+        </div>
+    </footer>
+</body>
+
+</html>
